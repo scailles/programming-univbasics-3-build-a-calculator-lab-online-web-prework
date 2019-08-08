60b6aa3297012b0bacd6b1b93edf48e5e4d0ffb1
celebrated-integer-2023 <scailles23@gmail.com>
2019-08-08 08:51:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  sum=num1+num2
+  print sum
 end
 
 def subtraction(num1, num2)
-
+  difference=num1-num2
+  print difference
 end
 
 def division(num1, num2)
-
+  num1%num2=quotient
+  print quotient
 end
 
 def multiplication(num1, num2)
-
+  product=num1**num2
+  print product
 end
 
 def modulo(num1, num2)
